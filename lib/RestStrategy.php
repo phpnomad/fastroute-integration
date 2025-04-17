8f29b88e5915b3ec62c17ec8efa1c047f2f739d4
Alex Standiford <alex@standiford.us>
2025-04-17 13:47:03 -0400

    Adds missing interceptor and middleware support

--- lib/RestStrategy.php
+++ lib/RestStrategy.php
@@ -3,12 +3,16 @@
 namespace PHPNomad\FastRoute\Component;
 
 use PHPNomad\FastRoute\Component\Registries\RestRoutesRegistry;
+use PHPNomad\Rest\Exceptions\ValidationException;
 use PHPNomad\Rest\Interfaces\Controller;
 use PHPNomad\Rest\Interfaces\HasInterceptors;
 use PHPNomad\Rest\Interfaces\HasMiddleware;
 use PHPNomad\Rest\Interfaces\HasValidations;
 use PHPNomad\Http\Interfaces\Response;
+use PHPNomad\Rest\Interfaces\Interceptor;
+use PHPNomad\Rest\Interfaces\Middleware;
 use PHPNomad\Rest\Interfaces\RestStrategy as CoreRestStrategy;
+use PHPNomad\Utils\Helpers\Arr;
 
 class RestStrategy implements CoreRestStrategy
 {
@@ -25,24 +29,21 @@ class RestStrategy implements CoreRestStrategy
             $controller = $controllerGetter();
 
             return [
-                $controller->getMethod(),
-                $controller->getEndpoint(),
-                function ($request) use ($controller) {
-                    if ($controller instanceof HasValidations) {
-                        $this->runValidations($controller, $request);
-                    }
-                    if ($controller instanceof HasMiddleware) {
-                        $this->runMiddleware($controller, $request);
-                    }
-                    $response = $controller->getResponse($request);
-                    $this->setRestHeaders($response);
+              $controller->getMethod(),
+              $controller->getEndpoint(),
+              function ($request) use ($controller) {
+                  if ($controller instanceof HasMiddleware) {
+                      $this->runMiddleware($controller, $request);
+                  }
+                  $response = $controller->getResponse($request);
+                  $this->setRestHeaders($response);
 
-                    if ($controller instanceof HasInterceptors) {
-                        $this->runInterceptors($controller, $request, $response);
-                    }
+                  if ($controller instanceof HasInterceptors) {
+                      $this->runInterceptors($controller, $request, $response);
+                  }
 
-                    return $response;
-                }
+                  return $response;
+              }
             ];
 
         });
@@ -70,39 +71,13 @@ class RestStrategy implements CoreRestStrategy
         // Add any other necessary headers here
     }
 
-    private function runValidations(Controller $controller, $request): void
-    {
-        /**
-         * @TODO
-         *
-         * This should run preflight validations based on the configurations sometimes found in the controller.
-         * If a controller implements HasValidations, we should implement it here.
-         *
-         * See WordPress integration for more info.
-         */
-    }
-
     private function runMiddleware(Controller $controller, $request): void
     {
-        /**
-         * @TODO
-         *
-         * This should run middleware based on the configurations sometimes found in the controller.
-         * If a controller implements HasMiddleware, we should implement it here.
-         *
-         * See WordPress integration for more info.
-         */
+        Arr::each($controller->getMiddleware($request), fn(Middleware $middleware) => $middleware->process($request));
     }
 
     private function runInterceptors(Controller $controller, $request, $response): void
     {
-        /**
-         * @TODO
-         *
-         * This should run interceptors based on the configurations sometimes found in the controller.
-         * If a controller implements HasInterceptors, we should implement it here.
-         *
-         * See WordPress integration for more info.
-         */
+        Arr::each($controller->getInterceptors($request, $response), fn(Interceptor $interceptor) => $interceptor->process($request, $response));
     }
 }
